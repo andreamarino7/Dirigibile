2f005c78337e0b609709dea7aad5d0dd8744073b
Andrea <andreamarino7@libero.it>
2020-07-17 17:03:40 +0200

    Inserita function per il calcolo della coordinata del baricentro e inserimento plot piano xz

--- Baricentro_Dirigibile.m
+++ Baricentro_Dirigibile.m
@@ -1,4 +1,4 @@
-function I = Inerzia_Dirigibile
+function xg = Baricentro_Dirigibile
 %% Calcolo momento di inerzia
 
 lc=65.4;        %lunghezza cilindro
@@ -13,8 +13,4 @@ mc=m0*Vc/(Vc+Vs);
 ms=m0-mc;
 xs=3*(R^4-r^4)/(8*(R^3-r^3));
 xg=(mc*lc/2+ms*(lc+xs))/m0;
-Ic=mc*(R^2+r^2)/4;
-Is=ms*(R^5-r^5)/(R^3-r^3)/5-ms*xs^2;
-Ip=mp*dp^2;
-I=Ic+mc*(xg-lc/2)^2+Is+ms*(lc+xs-xg)^2+10*Ip;
 end
